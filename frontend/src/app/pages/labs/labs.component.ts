882fcf7d1d94a4b2183a2b2825bec57397343fcf
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-06-14 11:13:45 +0200

    enrutado entre pag de angular

--- frontend/src/app/pages/labs/labs.component.ts
+++ frontend/src/app/pages/labs/labs.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-labs',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './labs.component.html',
   styleUrl: './labs.component.css'
 })
 export class LabsComponent {
-
+  title = 'Valor enlazado desde el componente AppComponent';
+  tareas=[
+    "Estudiar Angular",
+    "Estudiar Testing en Angular",
+    "Estudiar Vue.js",
+    "Estudiar Testing en Vue.js",
+    "Estudiar NestJS"
+  ]
 }
