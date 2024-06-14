970d65516b54cc391fa85d0456b0dbca46168e52
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-06-14 10:59:54 +0200

    forma de iterar e imprimir tareas

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule], //!tengo que agregar el CommonModule para que funcione el ngFor
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  welcome = 'Valor enlazado desde el componente AppComponent';
+  title = 'Valor enlazado desde el componente AppComponent';
+  tareas=[
+    "Estudiar Angular",
+    "Estudiar Testing en Angular",
+    "Estudiar Vue.js",
+    "Estudiar Testing en Vue.js",
+    "Estudiar NestJS"
+  ]
 }
