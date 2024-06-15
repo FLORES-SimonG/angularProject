22ac85ad27d7015a1bed744d9a080004141ca17a
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-06-15 20:46:30 +0200

    boton creado y utilizando variables directas

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -6,16 +6,18 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
   title = 'Valor enlazado desde el componente AppComponent';
-  tareas=[
-    "Estudiar Angular",
-    "Estudiar Testing en Angular",
-    "Estudiar Vue.js",
-    "Estudiar Testing en Vue.js",
-    "Estudiar NestJS"
-  ]
-
+  tareas = [
+    'Estudiar Angular',
+    'Estudiar Testing en Angular',
+    'Estudiar Vue.js',
+    'Estudiar Testing en Vue.js',
+    'Estudiar NestJS',
+  ];
+  name = 'Juan';
+  visibilidad = true;
+  img='https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png';
 }
