53cf831233c9f4274cbc41a60b27109c365b584f
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-06-15 20:13:18 +0200

    agregue tailwind

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,10 +6,10 @@ import { HomeComponent } from './pages/home/home.component';
 export const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
-  }
-  ,{
+    component: HomeComponent,
+  },
+  {
     path: 'labs',
-    component: LabsComponent
-  }
+    component: LabsComponent,
+  },
 ];
